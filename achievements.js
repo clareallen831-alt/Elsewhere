53e95f9cc2aa90f284087c21c4f292e2362b16c9
clareallen831-alt <clare.allen831@gmail.com>
2026-08-13 15:13:17 +0100

    Load achievement and sewing step enhancements

---
+++ achievements.js
@@ -0,0 +1,10 @@
+(() => {
+  const load=src=>new Promise((resolve,reject)=>{
+    const script=document.createElement('script');
+    script.src=src;
+    script.onload=resolve;
+    script.onerror=reject;
+    document.head.appendChild(script);
+  });
+  load('./achievements-base.js').then(()=>load('./project-steps.js')).catch(console.error);
+})();
