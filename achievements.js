929030b51fe0d052872a8d37d3f99bc2e4fbe875
clareallen831-alt <clare.allen831@gmail.com>
2026-08-15 17:09:07 +0100

    Load Eat history and backup features

--- achievements.js
+++ achievements.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION='20260815-foodlog2';
+  const VERSION='20260815-finish1';
   const load=src=>new Promise((resolve,reject)=>{
     const script=document.createElement('script');
     script.src=`${src}?v=${VERSION}`;
@@ -8,5 +8,6 @@
     document.head.appendChild(script);
   });
   load('./achievements-base.js').then(()=>load('./project-steps.js')).catch(console.error);
-  load('./food.js').then(()=>load('./recipe-calculator.js')).then(()=>load('./recipe-paste.js')).catch(console.error);
+  load('./food.js').then(()=>load('./recipe-calculator.js')).then(()=>load('./recipe-paste.js')).then(()=>load('./food-extras.js')).catch(console.error);
+  load('./backup.js').catch(console.error);
 })();
