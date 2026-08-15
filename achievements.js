e7670230a5d4b69ad8c89d65f17d9f6db4bf9386
clareallen831-alt <clare.allen831@gmail.com>
2026-08-15 13:25:37 +0100

    Load food tracker

--- achievements.js
+++ achievements.js
@@ -6,5 +6,8 @@
     script.onerror=reject;
     document.head.appendChild(script);
   });
-  load('./achievements-base.js').then(()=>load('./project-steps.js')).catch(console.error);
+  load('./achievements-base.js')
+    .then(()=>load('./project-steps.js'))
+    .then(()=>load('./food.js'))
+    .catch(console.error);
 })();
