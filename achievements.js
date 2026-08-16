1d28b129839d9db714cc7f20a424e5c30e78e989
clareallen831-alt <clare.allen831@gmail.com>
2026-08-16 12:24:21 +0100

    Load quick favourite food action

--- achievements.js
+++ achievements.js
@@ -1,5 +1,5 @@
 (() => {
-  const VERSION='20260816-favourites2';
+  const VERSION='20260816-favourites3';
   const load=src=>new Promise((resolve,reject)=>{
     const script=document.createElement('script');
     script.src=`${src}?v=${VERSION}`;
@@ -8,6 +8,6 @@
     document.head.appendChild(script);
   });
   load('./achievements-base.js').then(()=>load('./project-steps.js')).catch(console.error);
-  load('./food.js').then(()=>load('./recipe-calculator.js')).then(()=>load('./recipe-paste.js')).then(()=>load('./food-extras.js')).then(()=>load('./food-favourites-v2.js')).catch(console.error);
+  load('./food.js').then(()=>load('./recipe-calculator.js')).then(()=>load('./recipe-paste.js')).then(()=>load('./food-extras.js')).then(()=>load('./food-favourites-v2.js')).then(()=>load('./food-favourite-quick.js')).catch(console.error);
   load('./backup.js').catch(console.error);
 })();
